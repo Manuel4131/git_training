e635213e9d905f52082f6be0452518b0fbfe7a3e
Manuel4131 <Manual131419@gmail.com>
2018-07-22 20:13:55 +0800

    garbage word

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@
 import ftime
 
 def weather():
-    city = "London"	# Change Berlin to London
+    city = "London"# Change Berlin to London
     print("Here is {}".format(city))
 	print("Current date and time are {}".format(ftime.today()))
 
