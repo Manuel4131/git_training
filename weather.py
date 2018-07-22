4ecb41347d29ff47d4f4b87f1ed972ca864701b2
Manuel4131 <Manual131419@gmail.com>
2018-07-22 20:25:58 +0800

    remove incorrect format

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import ftime
 def weather():
     city = "London"# Change Berlin to London
     print("Here is {}".format(city))
-	print("Current date and time are {}".format(ftime.today()))
+    print("Current date and time are {}".format(ftime.today()))
 
 if __name__ == "__main__":
-	weather()
+    weather()
